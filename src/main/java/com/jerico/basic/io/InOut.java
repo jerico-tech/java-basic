0892e038a75b835302cc6844d3acf1acde488e1f
hairui <hairui1986sd@163.com>
2019-08-11 23:31:09 +0800

    init project

---
+++ src/main/java/com/jerico/basic/io/InOut.java
@@ -0,0 +1,14 @@
+package com.jerico.basic.io;
+
+/**
+ * @className: InOut
+ * @description:
+ * @author: jerico
+ * @date: 2019-08-11 23:18
+ * @version:
+ */
+public class InOut {
+    public static void main(String[] args){
+        System.out.println("i am println");
+    }
+}
